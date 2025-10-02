27da93d6579f543a9066e17fb1c9b3a6e04c7522
RonnelR <91006865+RonnelR@users.noreply.github.com>
2025-10-02 22:44:51 +0530

    fix errors

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ const app = express();
 
 const allowedOrigins = [
   "http://localhost:3000",         // local dev
-  "https://effervescent-shortbread-431c3b.netlify.app"  // deployed frontend
+  "https://italics-blogging-platform.netlify.app"  // deployed frontend
 ];
 
 //middleware
